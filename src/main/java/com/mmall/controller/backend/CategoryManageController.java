647d62decb61e0905dd962355742644a6d0efab3
wangzehua <wzhxingsu1202@163.com>
2018-01-30 14:07:59 +0800

    6-3 查询节点
    通过递归查找本节点及所有子节点的id接口开发

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -37,7 +37,7 @@ public class CategoryManageController {
             //是管理员
             return iCategoryService.addCategory(categoryName, parentID);
         }else{
-            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限")
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
         }
     }
 
@@ -54,7 +54,42 @@ public class CategoryManageController {
             //更新category
             return iCategoryService.updateCategoryName(categoryId, categoryName);
         }else{
-            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限")
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
         }
     }
+
+    @RequestMapping("get_category.do")
+    @ResponseBody
+    public ServerResponse getChildrenParallelCategory(HttpSession session,@RequestParam(value = "categoryId", defaultValue = "0") Integer categoryId){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录");
+        }
+
+        //校验是否是管理员
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //查询子节点的category信息，并且不递归，保持平级
+            return iCategoryService.getChildrenParallelCategory(categoryId);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
+        }
+    }
+
+    @RequestMapping("get_deep_category.do")
+    @ResponseBody
+    public ServerResponse getCategoryAndDeepChildrenCategory(HttpSession session,@RequestParam(value = "categoryId", defaultValue = "0") Integer categoryId){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录");
+        }
+
+        //校验是否是管理员
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //查询当前节点的id和递归子节点的id
+            return iCategoryService.selectCategoryAndChildrenById(categoryId);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
+        }
+    }
+
 }
--- src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
@@ -1,15 +1,25 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.mmall.commom.ServerResponse;
 import com.mmall.dao.CategoryMapper;
 import com.mmall.pojo.Category;
 import com.mmall.service.ICategoryService;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Set;
 
 @Service("iCategoryService")
 public class CategoryServiceImpl implements ICategoryService {
+
+    private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
     @Autowired
     private CategoryMapper categoryMapper;
@@ -50,4 +60,44 @@ public class CategoryServiceImpl implements ICategoryService {
         }
     }
 
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId) {
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        if (CollectionUtils.isEmpty(categoryList)) {
+            logger.info("未找到当前分类的子类");
+        }
+        return ServerResponse.createBySuccess(categoryList);
+    }
+
+    /**
+     * 递归查询本节点的id以及孩子节点的id
+     * @param categoryId
+     * @return
+     */
+    public ServerResponse selectCategoryAndChildrenById(Integer categoryId) {
+        Set<Category> categorySet = Sets.newHashSet();
+        findChildCategory(categorySet, categoryId);
+
+        List<Integer> categoryIdList = Lists.newArrayList();
+        if (categoryId != null) {
+            for (Category categoryItem : categorySet) {
+                categoryIdList.add(categoryItem.getId());
+            }
+        }
+        return ServerResponse.createBySuccess(categoryIdList);
+    }
+
+    //递归算法，算出子节点
+    private Set<Category> findChildCategory(Set<Category> categorySet, Integer categoryId) {
+        Category category = categoryMapper.selectByPrimaryKey(categoryId);
+        if (category != null) {
+            categorySet.add(category);
+        }
+        //查找子节点
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        //mybatis 不会返回null，所以这里不用对categoryList进行是否为空的判断
+        for (Category categoryItem : categoryList) {
+            findChildCategory(categorySet, categoryItem.getId());
+        }
+        return categorySet;
+    }
 }
