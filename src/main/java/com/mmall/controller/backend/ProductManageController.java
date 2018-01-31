06c5eb53093dab7df4aa83ff9ad3a869e0cd9f54
wangzehua <wzhxingsu1202@163.com>
2018-01-31 15:55:54 +0800

    7-4 后台获取商品列表
    后台获取商品列表
    动态分页查询

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -11,6 +11,7 @@ import com.mmall.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
@@ -83,6 +84,22 @@ public class ProductManageController {
         }
         if (iUserService.checkAdminRole(user).isSuccess()) {
             return iProductService.manageProductDetail(productId);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse getList(HttpSession session, @RequestParam(value="pageNum", defaultValue = "1") int pageNum,
+                                  @RequestParam(value="pageSize", defaultValue = "10") int pageSize) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录后重试");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iProductService.getProductList(pageNum, pageSize);
         } else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
